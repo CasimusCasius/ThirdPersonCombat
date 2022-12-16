adf0bad052d91c925db4876e2e4c9da25c5bb7e8
CasimusCasius <grzegorz.kazimierczak.sg@gmail.com>
2022-12-16 11:02:16 +0100

    InputSystem Events

--- Assets/Scripts/StateMachines/Player/PlayerStateTest.cs
+++ Assets/Scripts/StateMachines/Player/PlayerStateTest.cs
@@ -1,30 +1,32 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerStateTest : PlayerBaseState
 {
-    private float timeToSwitch = 5f;
+    private float timeSinceJump = 0f;
     public PlayerStateTest(PlayerStateMachine stateMachine) : base(stateMachine)
     {
     }
     public override void Enter()
     {
-        Debug.Log("Enter");
+        
+        stateMachine.InputReader.JumpEvent += OnJump;
     }
     public override void Tick(float deltaTime)
     {
-        timeToSwitch -= Time.deltaTime;
-        Debug.Log(timeToSwitch);
-        if (timeToSwitch <= 0f)
-        {
-            stateMachine.SwitchState(new PlayerStateTest(stateMachine));
-            timeToSwitch = 5f;
-        }
-
+        timeSinceJump += Time.deltaTime;
+        Debug.Log(timeSinceJump);
     }
     public override void Exit()
     {
-        Debug.Log("Exit");
+
+        stateMachine.InputReader.JumpEvent -= OnJump;
+    }
+    private void OnJump()
+    {
+        stateMachine?.SwitchState(new PlayerStateTest(stateMachine));
+        timeSinceJump = 0f;
     }
 }
